ba7636c842665f8e335bcaef5bc114c78ce8d86d
zhuzhuxia <zhuzhuxia@163.com>
2017-10-22 10:16:38 +0800

    项目目录创建

---
+++ src/components/blank.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header-top></header-top>
+    <p is="footer-bottom" :number="num" @a-target="animateX"></p>
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+  import headerTop from './headerTop';
+  import footerBottom from './footer';
+
+  export default {
+    data() {
+      return {
+        num:90
+      }
+    },
+    components: {
+      headerTop: headerTop,
+      footerBottom
+    },
+    methods:{
+      animateX(target){
+        console.log(target);
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="footer">
+    {{number}}
+    <button @click="animationTarget"></button>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      'number': {
+        type: Number
+      }
+    },
+    methods: {
+      animationTarget(e) {
+        this.$emit('a-target', e.target);
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+.footer{
+  button{
+    display: block
+    width: 100px
+    height: 30px
+    background: #00a0dc
+  }
+}
+</style>
